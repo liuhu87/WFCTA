876c244282654454a6019e689df1424c1d091da5
youzhiyong <youzhiyong@lxslc608.ihep.ac.cn>
2019-05-20 18:59:05 +0800

    modified:   CC/WFCTAEvent.cc 	modified:   include/WFCTAEvent.h

--- include/WFCTAEvent.h
+++ include/WFCTAEvent.h
@@ -1,87 +1,62 @@
 #ifndef WFCTAEVENT_H
 #define WFCTAEVENT_H
 
-#include <stdint.h>
-#include <stdlib.h>
 #include <vector>
+#include <string>
 #include <map>
 #include "TObject.h"
 
 using namespace std;
-
-class WFCTAEvent//: public TObject
+class WFCTAEvent //: public TObject 
 {
 public:
-    WFCTAEvent();
-    ~WFCTAEvent();
+        WFCTAEvent();
+        ~WFCTAEvent();
+        void EventInitial();
+	void StatusInitial();
+public:
+        long iEvent;
+        long rabbitTime;
+        double rabbittime;
+        vector<short> iSiPM;
+	vector<bool> gain_marker;
+	vector<char> peak;
+	vector<char> mypeak;
+	vector<short> Single_Threshold;
+	vector<short> Record_Threshold;
+	vector<bool> Over_Single_Marker;
+	vector<bool> Over_Record_Marker;
+	vector<float> ADC_Cut;
+	vector<float> ImageBaseHigh;
+	vector<float> ImageBaseLow;
+	vector<float> ImageAdcHigh;
+	vector<float> ImageAdcLow;
+	vector<float> myImageBaseHigh;
+	vector<float> myImageBaseLow;
+	vector<float> myImageAdcHigh;
+	vector<float> myImageAdcLow;
+	void SetEvent(long event);
+	void SetrabbitTime(long rabTime);
+	void Setrabbittime(double rabtime);
+        void SetSiPM(short SiPM);
+        void Setgain_marker(bool w_gain_marker);
+        void Setpeak(char w_peak);
+        void Setmypeak(char w_mypeak);
+        void SetSingle_Threshold(short w_Single_Threshold);
+        void SetRecord_Threshold(short w_Record_Threshold);
+        void SetOver_Single_Marker(bool w_Over_Single_Marker);
+        void SetOver_Record_Marker(bool w_Over_Record_Marker);
+        void SetADC_Cut(float w_ADC_Cut);
+        void SetImageBaseHigh(float w_ImageBaseHigh);
+        void SetImageBaseLow(float w_ImageBaseLow);
+        void SetImageAdcHigh(float w_ImageAdcHigh);
+        void SetImageAdcLow(float w_ImageAdcLow);
+        void SetmyImageBaseHigh(float w_myImageBaseHigh);
+        void SetmyImageBaseLow(float w_myImageBaseLow);
+        void SetmyImageAdcHigh(float w_myImageAdcHigh);
+        void SetmyImageAdcLow(float w_myImageAdcLow);
 
-    uint8_t StatusPackCheck(uint8_t *begin, int bufsize);
-
-    bool bigPackCheck(uint8_t *begin, int bufsize);
-    void Find_SiPMs(uint8_t *begin, int packsize);
-
-    uint64_t PackSize() {return packSize;};
-    map<short, int>& GetSiPM_Position() {return m_sipm_position;};
-
-    uint64_t eventId(uint8_t *begin);
-    uint64_t RabbitTime(uint8_t *begin);
-    uint64_t Rabbittime(uint8_t *begin);
-
-    uint8_t GetPeak(uint8_t *begin, short isipm);
-    bool Getgain_marker(uint8_t *begin, short isipm);
-    uint8_t Getmypeak(uint8_t *begin, short isipm);
-    uint16_t GetSingle_Thresh(uint8_t *begin, short isipm);
-    uint16_t GetRecord_Thresh(uint8_t *begin, short isipm);
-    bool GetOver_Single_Mark(uint8_t *begin, short isipm);
-    bool GetOver_Record_Mark(uint8_t *begin, short isipm);
-    float GetADC_Cut(uint8_t *begin, short isipm);
-    float AdcHigh(uint8_t *begin, short isipm);
-    float AdcLow(uint8_t *begin, short isipm);
-    float BaseHigh(uint8_t *begin, short isipm);
-    float BaseLow(uint8_t *begin, short isipm);
-    float GetmyImageBaseHigh(uint8_t *begin, short isipm);
-    float GetmyImageBaseLow(uint8_t *begin, short isipm);
-    float GetmyImageAdcHigh(uint8_t *begin, short isipm);
-    float GetmyImageAdcLow(uint8_t *begin, short isipm);
-    void GetWaveForm(uint8_t *begin, short isipm, int *pulseh, int *pulsel);
-    
-
-    void Deal81Package(uint8_t *begin, int packsize, float *HV);
-    void Deal82Package(uint8_t *begin, int packsize, float *PreTemp);
-
-    uint64_t GetStatusReadbackTime(uint8_t *begin, int packsize);
-    uint64_t GetStatusReadbacktime(uint8_t *begin, int packsize);
-private:
-
-    void waveform(uint8_t *begin, short isipm);
-    void Calc_Q_Base(uint8_t *begin, short isipm);
-
-    //uint8_t *m_buffer;
-    //size_t m_size;
-    int64_t head,tail;
-    int64_t readPos;
-
-    uint8_t status_pack_mark;
-    int64_t packSize;
-    map<short, int> m_sipm_position;
-    map<short, int>::iterator m_sipm_position_iter;
-
-    //char m_peak;
-    float m_adc_high;
-    float m_adc_low;
-    float m_base_high;
-    float m_base_low;
-
-    float m_Basehigh;
-    float m_Baselow;
-    float m_Adchigh;
-    float m_Adclow;
-
-    uint8_t m_mypeak;
-
-    int pulsehigh[32];
-    int pulselow[32];
-   //ClassDef(WFCTAEvent,1);
+   ClassDef(WFCTAEvent,0);
 };
 
 #endif // WFCTAEVENT_H
