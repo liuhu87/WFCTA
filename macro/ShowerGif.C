f7d24fcf4273fa595035636283ea9c8378ff71e3
huliu <huliu@localhost.localdomain>
2019-08-09 15:02:15 +0800

    modified:   macro/ShowerGif.C

--- macro/ShowerGif.C
+++ macro/ShowerGif.C
@@ -1,5 +1,5 @@
 {
-   //gSystem->Load("lib/lib.so");
+   gSystem->Load("lib/lib.so");
    ShowerPlot::jdebug=6;
    ReadTrack::jdebug=0;
    ReadTrack::DoPlot=true;
@@ -13,9 +13,11 @@
    //char* type=argc[2];
    //int run=atoi(argc[3]);
 
-   char* dir="/home/huliu/Downloads/ShowerImageData/electron/1E14";
-   char* type="electron";
+   const char* dir="/home/huliu/Downloads/ShowerImageData/electron/1E14";
+   const char* type="electron";
    int run=2;
+   bool deterrange=true;
+   int ipng=0;
 
    ShowerPlot* gplot=new ShowerPlot(type);
    gplot->Add(Form("%s/DAT%06d.track_em",dir,run),0);
@@ -25,38 +27,52 @@
    //printf("Adding %s/DAT%06d.track_em\n",dir,run);
 
    //define the range
-   gplot->Read();
-   TCanvas* c0=gplot->Draw(0);
-   if(c0) c0->SetName("temp_canvas");
-   double coomax=0,zcoomax=gplot->plotrange[2][1];
-   double tminmax[2]={gplot->plotrange[3][0]*0.9,gplot->plotrange[3][1]*1.1};
-   for(int ii=0;ii<2;ii++){
-      if(fabs(gplot->plotrange[ii][0])>coomax) coomax=fabs(gplot->plotrange[ii][0]);
-      if(fabs(gplot->plotrange[ii][1])>coomax) coomax=fabs(gplot->plotrange[ii][1]);
+   double coomax,zcoomax;
+   double tminmax[2];
+   if(deterrange){
+      gplot->Read();
+      TCanvas* c0=gplot->Draw(0);
+      if(c0) c0->SetName("temp_canvas");
+      coomax=0; zcoomax=gplot->plotrange[2][1];
+      tminmax[0]=gplot->plotrange[3][0]*0.9; tminmax[1]=gplot->plotrange[3][1]*1.1;
+      for(int ii=0;ii<2;ii++){
+         if(fabs(gplot->plotrange[ii][0])>coomax) coomax=fabs(gplot->plotrange[ii][0]);
+         if(fabs(gplot->plotrange[ii][1])>coomax) coomax=fabs(gplot->plotrange[ii][1]);
+      }
+      coomax*=1.05;
+      zcoomax*=1.05;
+      printf("XY Max=%f, Z Max=%f, Time Range={%f,%f}\n",coomax,zcoomax,tminmax[0],tminmax[1]);
+      return 1;
    }
-   coomax*=1.05;
-   zcoomax*=1.05;
+
+   coomax=514724.076563;
+   zcoomax=11847064.950000;
+   tminmax[0]=0;
+   tminmax[1]=0.000543;
+
    ReadTrack::IniRange[0][0]=-coomax;
    ReadTrack::IniRange[0][1]=coomax;
    ReadTrack::IniRange[1][0]=-coomax;
    ReadTrack::IniRange[1][1]=coomax;
-   ReadTrack::IniRange[2][0]=0;
+   ReadTrack::IniRange[2][0]=-1.e4;
    ReadTrack::IniRange[2][1]=zcoomax;
-   printf("XY Max=%f, Z Max=%f, Time Range={%f,%f}\n",coomax,zcoomax,tminmax[0],tminmax[1]);
 
    //Draw png plot
+   if(ipng<0){
    printf("Begin to draw png plot\n");
    gplot->Read();
    TCanvas* cc=gplot->Draw(0);
    if(cc) cc->SetName("png_canvas");
    if(cc) cc->SaveAs(Form("%s/plot.png",dir));
+   return 1;
+   }
 
    //Draw gif plot
    printf("Begin to draw gif plot\n");
    const int nt=10;
    ReadTrack::tlimit[0]=-1;
    for(int ii=0;ii<nt;ii++){
-   ReadTrack::jdebug=ii==0?0:0;
+      if(ipng>=0&&ipng!=ii) continue;
       double tmax=tminmax[0]+(tminmax[1]-tminmax[0])/nt*(ii+0.5);
       printf("Draw %d tt=%lf\n",ii,tmax);
       ReadTrack::tlimit[1]=tmax;
